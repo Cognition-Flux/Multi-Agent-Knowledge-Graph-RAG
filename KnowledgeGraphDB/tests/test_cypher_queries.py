0b4e765968efe46ac928951ed8da2e6c7d61b5dd
alejandroacevedo2305 <a.acevedo.aracena@gmail.com>
2025-08-06 11:15:11 -0400

    aumentando ejemplos de queries

--- KnowledgeGraphDB/tests/test_cypher_queries.py
+++ KnowledgeGraphDB/tests/test_cypher_queries.py
@@ -8,7 +8,7 @@ import yaml
 from KnowledgeGraphDB.Neo4j_KG_creation.cypher_runner import run_cypher
 
 
-yaml_path = Path(__file__).with_name("sample_queries.yaml")
+yaml_path = Path(__file__).with_name("sample_queries_cypher_agent.yaml")
 
 
 sample_queries = yaml.safe_load(yaml_path.read_text(encoding="utf-8"))
